11135ac16bc9b1bedb85d88046259a123ad23369
Ilkin Mammadzada <ilkindigo.ca@gmail.com>
2024-10-22 15:46:17 -0400

    Create userController.js

---
+++ src/controllers/userController.js
@@ -0,0 +1,23 @@
+const User = require('../models/users');
+
+exports.getUserById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const user = await User.findById(id);
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch user' });
+    }
+};
+
+exports.getAllUsers = async (req, res) => {
+    try {
+        const users = await User.findAll();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch users' });
+    }
+};
